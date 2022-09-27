fe887968677c67adbff3eb5edbd77d6b6cf10d7f
r6ooo <114106384+r6ooo@users.noreply.github.com>
2022-09-27 19:12:42 +0300

    tt

--- Fundamentals/fun/rightPlace.js
+++ Fundamentals/fun/rightPlace.js
@@ -2,7 +2,7 @@ function replace (str,char,result){
     let after = str.replace('_',char);
     
     let outPut = after === result? "Matched": "Not Matched";
- //f
+
     console.log(outPut);
 }
 replace('Str_ng', 'i', 'String');
