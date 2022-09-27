73cdf80243a8739a11e80f02a7422d6276ffeadc
r6ooo <114106384+r6ooo@users.noreply.github.com>
2022-09-27 21:19:34 +0300

    7

---
+++ Fundamentals/DataTypesAndVariables/lowerOrUpper.js
@@ -0,0 +1,7 @@
+function dataDemo(character) {
+    let result = character === character.toUpperCase() ?
+        console.log('upper-case') :
+        console.log('lower-case');
+
+}
+dataDemo('d');
