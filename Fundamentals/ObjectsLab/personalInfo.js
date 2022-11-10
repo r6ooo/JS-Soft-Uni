6a59fdb5f980f8ad5bcfb962917264b95f846528
r6ooo <114106384+r6ooo@users.noreply.github.com>
2022-11-10 17:22:24 +0200

    1

---
+++ Fundamentals/ObjectsLab/personalInfo.js
@@ -0,0 +1,10 @@
+function personinfo(firstName, lastName, age) {
+
+    let person = {};
+    person.firstName = firstName;
+    person.lastName = lastName;
+    person.age = age;
+
+    return person;
+}
+personinfo("Peter", "Pan","20");
