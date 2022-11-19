b5d8c571bda7493be79e7082b6f10056b892beb8
r6ooo <114106384+r6ooo@users.noreply.github.com>
2022-11-19 19:18:34 +0200

    5

---
+++ Fundamentals/ObjectsExersays/inventory.js
@@ -0,0 +1,26 @@
+function heroesRegister(array) {
+
+    let heroes = [];
+
+    array.forEach(line => {
+        let [name, level, items] = line.split(' / ');
+        let currentHero = {
+            name,
+            level: Number(level),
+            items
+        }
+        heroes.push(currentHero)
+    });
+    heroes.sort((a, b) => a.level - b.level);
+
+    heroes.forEach((hero) => {
+        console.log(`Hero: ${hero.name}`);
+        console.log(`level => ${hero.level}`);
+        console.log(`items => ${hero.items}`);
+    })
+
+}
+heroesRegister(['Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara'
+]);
