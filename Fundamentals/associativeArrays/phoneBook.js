a78ba0f55daab57e2c1f5392edb1eb38ee3db035
r6ooo <114106384+r6ooo@users.noreply.github.com>
2022-11-20 18:59:52 +0200

    1

---
+++ Fundamentals/associativeArrays/phoneBook.js
@@ -0,0 +1,18 @@
+function printPhoneBook(input) {
+
+    let phoneBook = {};
+
+    for (let line of input) {
+
+        let [name, phone] = line.split(' ');
+        phoneBook[name] = phone;
+    }
+    for (let name in phoneBook) {
+        console.log(`${name} -> ${phoneBook[name]}`);
+    }
+
+}
+printPhoneBook(['Tim 0834212554',
+    'Peter 0877547887',
+    'Bill 0896543112',
+    'Tim 0876566344']);
