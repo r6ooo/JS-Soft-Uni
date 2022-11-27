ecfdfa6cef6e60c27ab23159bda0fd5f47fec664
r6ooo <114106384+r6ooo@users.noreply.github.com>
2022-11-27 18:25:30 +0200

    6

---
+++ Fundamentals/TextProcessing/pascalCaseSplitter.js
@@ -0,0 +1,14 @@
+function splitter(text) {
+
+    let result = text[0];
+    let lower = text.toLowerCase();
+
+    for (let index = 1; index < text.length; index++) {
+        if(text[index] !== lower[index]){
+            result += ', ';
+        }
+        result += text[index];
+    }
+    console.log(result);
+}
+splitter('SplitMeIfYouCanHaHaYouCantOrYouCan');
