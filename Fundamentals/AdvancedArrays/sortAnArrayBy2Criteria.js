11478568c42581bd246981726e9f92a89dd5c2ea
r6ooo <114106384+r6ooo@users.noreply.github.com>
2022-11-28 13:31:55 +0200

    5

---
+++ Fundamentals/AdvancedArrays/sortAnArrayBy2Criteria.js
@@ -0,0 +1,9 @@
+function sortArr(arr) {
+
+    let sortedArr = arr.sort((a, b) => a.length - b.length || a.localeCompare(b));
+
+    console.log(sortedArr.join('\n'));
+
+}
+sortArr(['alpha', 'beta', 'gamma']);
+sortArr(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']);
