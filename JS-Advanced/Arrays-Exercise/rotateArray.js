6defbbf6cc6e291ac85fa6ce36a6edbae52522cd
r6ooo <114106384+r6ooo@users.noreply.github.com>
2023-01-20 14:49:37 +0200

    4

---
+++ JS-Advanced/Arrays-Exercise/rotateArray.js
@@ -0,0 +1,9 @@
+function rotate(arr, rotations) {
+    for (let i = 0; i < rotations; i++) {
+        arr.unshift(arr.pop());
+    }
+    console.log(arr.join(' '));
+
+}
+rotate(['1', '2', '3', '4'], 2);
+rotate(['Banana','Orange','Coconut','Apple'],15);
