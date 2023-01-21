4d554e41094d4a8220be9f17eb755a2e252cb2d9
r6ooo <114106384+r6ooo@users.noreply.github.com>
2023-01-21 17:36:13 +0200

    8

---
+++ JS-Advanced/Arrays-Exercise/sortBy2Criteria.js
@@ -0,0 +1,6 @@
+function sorting(arr) {
+    arr.sort((a, b) => a.length - b.length || a.localeCompare(b));
+    console.log(arr.join('\n'));
+}
+sorting(['alpha','beta','gamma']);
+sorting(['test','Deny','omen','Default']);
