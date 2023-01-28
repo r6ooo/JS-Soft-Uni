bb8f0130b86345803e22b67c74d6d487c324213b
r6ooo <114106384+r6ooo@users.noreply.github.com>
2023-01-28 17:10:10 +0200

    8

---
+++ JS-Advanced/Objects and Composition-Exercise/rectange.js
@@ -0,0 +1,18 @@
+
+function rectangle(width, height, color) {
+    return {
+    width: width,
+    height: height,
+    color: color.charAt(0).toUpperCase() + color.slice(1),
+    calcArea: function () {
+        return Number(this.width * this.height);
+         
+    }}
+}
+
+
+let rect = rectangle(4, 5, 'red');
+console.log(rect.width);
+console.log(rect.height);
+console.log(rect.color);
+console.log(rect.calcArea());
