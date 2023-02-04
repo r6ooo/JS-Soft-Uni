7e1b94dc4f17d2baff8d43bb136eb23f7460cbb5
r6ooo <114106384+r6ooo@users.noreply.github.com>
2023-02-04 22:04:54 +0200

    3

--- JS-Advanced/DOM/03.Sum Numbers/SumNumbers.js
+++ JS-Advanced/DOM/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,7 @@
 function calc() {
-    // TODO: sum = num1 + num2
+    num1 = Number(document.getElementById('num1').value)
+    num2 = Number(document.getElementById('num2').value)
+    result = document.getElementById('sum');
+    sum = num1 + num2;
+    result.value = sum;
 }
