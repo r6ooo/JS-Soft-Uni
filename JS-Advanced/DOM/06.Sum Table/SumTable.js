ebfa43a599cfddea9e76620f790918e28fdd096a
r6ooo <114106384+r6ooo@users.noreply.github.com>
2023-02-06 17:46:30 +0200

    6

--- JS-Advanced/DOM/06.Sum Table/SumTable.js
+++ JS-Advanced/DOM/06.Sum Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
-
+    let table = document.querySelectorAll('table tr');
+    let sum = 0;
+    for (let index = 1; index < table.length; index++) {
+        let cols = table[index].children;
+        let price = cols[cols.length - 1].textContent;
+        sum += Number(price);
+    }
+    document.getElementById('sum').textContent = sum;
 }
