6efe55e4ab232bbee70e0ab318ace2396acbd81f
r6ooo <114106384+r6ooo@users.noreply.github.com>
2023-02-06 17:35:16 +0200

    5

--- JS-Advanced/DOM/05.Colorize Table/ColorizeTable.js
+++ JS-Advanced/DOM/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,10 @@
 function colorize() {
-    // TODO
+    const rows = document.querySelectorAll('table tr');
+    let index =0;
+    for (const row of rows) {
+        index++;
+        if (index % 2 == 0) {
+            row.style.background = 'teal';   
+        }    
+    }
 }
