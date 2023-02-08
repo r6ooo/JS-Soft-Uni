27e66d37cdbb0735c16da422942b5533e0249984
r6ooo <114106384+r6ooo@users.noreply.github.com>
2023-02-08 17:59:03 +0200

    2

--- JS-Advanced/DOM-Exercize/02. Pascal or Camel Case/solution.js
+++ JS-Advanced/DOM-Exercize/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,23 @@
 function solve() {
-  //TODO...
+  const string = document.getElementById('text').value;
+  const currCase = document.getElementById('naming-convention').value;
+  const result = document.getElementById('result');
+
+  const toPascal = (text) => text
+    .split(' ')
+    .map((w) => w[0].toUpperCase() + w.slice(1).toLowerCase())
+    .join('');
+
+  const toCamel = (text) => {
+    text = toPascal(text);
+    return text[0].toLowerCase() + text.slice(1);
+  }
+  if (currCase === 'Pascal Case') {
+    result.textContent = toPascal(string);
+  } else if (currCase === 'Camel Case') {
+    result.textContent = toCamel(string);
+  } else {
+    result.textContent = 'Error!'
+  }
+
 }
