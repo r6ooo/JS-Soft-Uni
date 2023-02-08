2fdc3a850690eefc3462094b0483b8916f45be4a
r6ooo <114106384+r6ooo@users.noreply.github.com>
2023-02-08 20:34:31 +0200

    4

--- JS-Advanced/DOM-Exercize/04. Search in List/search.js
+++ JS-Advanced/DOM-Exercize/04. Search in List/search.js
@@ -1,3 +1,19 @@
 function search() {
-   // TODO
+   const list = Array.from(document.querySelectorAll('#towns li'));``
+   const search = document.getElementById('searchText');
+   const result = document.querySelector('#result');
+
+   let counter = 0;
+
+   list.forEach((element) => {
+      if (element.textContent.includes(search.value)) {
+         element.style.fontWeight = 'bold';
+         element.style.textDecoration = 'underline';
+         counter++;
+      } else {
+         element.style.fontWeight = 'normal';
+         element.style.textDecoration = 'none';
+      }
+   });
+   result.textContent = `${counter} matches found`
 }
