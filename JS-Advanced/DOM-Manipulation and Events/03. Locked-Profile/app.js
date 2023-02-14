7798b1df6f8855d1a0bbd05ffb1559c7e59e7a04
r6ooo <114106384+r6ooo@users.noreply.github.com>
2023-02-14 19:31:37 +0200

    3

--- JS-Advanced/DOM-Manipulation and Events/03. Locked-Profile/app.js
+++ JS-Advanced/DOM-Manipulation and Events/03. Locked-Profile/app.js
@@ -1,3 +1,19 @@
 function lockedProfile() {
-    console.log('TODO...')
+    Array.from(document.querySelectorAll('.profile button')).forEach((button) =>
+        button.addEventListener('click', onClick));
+
+    function onClick(event) {
+        const parent = event.target.parentElement;
+        const unlockedCheck = parent.querySelector('input[value="unlock"]');
+        if (unlockedCheck.checked) {
+            const hiddenDiv = parent.querySelector('div');
+            hiddenDiv.style.display === 'block'
+                ? (hiddenDiv.style.display = 'none')
+                : (hiddenDiv.style.display = 'block');
+
+            event.target.textContent === 'Show more'
+                ? (event.target.textContent) = 'Hide it'
+                : (event.target.textContent) = 'Show more';
+        }
+    }
 }
