351c320b5ee8f823f55bea7a62d6e3190a6fa8bb
r6ooo <114106384+r6ooo@users.noreply.github.com>
2023-02-15 19:09:43 +0200

    6

--- JS-Advanced/DOM-Exercize/06. Format the Text/solution.js
+++ JS-Advanced/DOM-Exercize/06. Format the Text/solution.js
@@ -1,3 +1,26 @@
 function solve() {
-  //TODO
+  
+  const input = document.getElementById('input');
+  const output = document.querySelector('#output');
+  let counter = 0;
+  let out = '';
+  const text = input.value.split('.').filter((e) => e.length > 0);
+  debugger
+  for (let i = 0; i < text.length; i++) {
+    if (counter == 0) {
+      out += '<p>'
+    }
+    else if (counter == 3 || i == text.length) {
+      out += '</p>';
+      counter = 0;
+      i--;
+      continue;
+    }
+    if (counter < 3) {
+      out += text[i] + '.';
+    }
+    counter++;
+  }
+  output.innerHTML = out;
+
 }
