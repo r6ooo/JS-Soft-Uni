f87fa7e6ca56c57d30996482b194855690811568
r6ooo <114106384+r6ooo@users.noreply.github.com>
2023-02-16 14:25:45 +0200

    1

---
+++ JS-Advanced/Advanced-Functions/sortArray.js
@@ -0,0 +1,8 @@
+function solve(arr, order) {
+    return order === 'asc' 
+    ? arr.sort((a, b) => a - b) 
+    : arr.sort((a,b) => b - a);
+
+}
+solve([14, 7, 17, 6, 8], 'asc');
+solve([14, 7, 17, 6, 8], 'desc');
