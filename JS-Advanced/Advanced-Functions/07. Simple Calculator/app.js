9db3bef080de0d8610d8fa0864e765d149c7d2d9
r6ooo <114106384+r6ooo@users.noreply.github.com>
2023-02-16 21:00:29 +0200

    7

--- JS-Advanced/Advanced-Functions/07. Simple Calculator/app.js
+++ JS-Advanced/Advanced-Functions/07. Simple Calculator/app.js
@@ -1,7 +1,18 @@
 function calculator() {
-    // TODO:
+    let num1;
+    let num2;
+    let res;
+
+    return {
+        init: (selector1, selector2, result) => {
+            num1 = document.querySelector(selector1);
+            num2 = document.querySelector(selector2);
+            res = document.querySelector(result);
+        },
+        add: () => (res.value = +num1.value + +num2.value),
+        subtract: () => (res.value = +num1.value - +num2.value)
+    };
 }
 
-
-
-
+const calculate = calculator (); 
+calculate.init ('#num1', '#num2', '#result'); 
